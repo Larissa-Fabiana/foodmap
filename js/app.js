a31c28707d704ea769272b62955c1ab348f1e7fb
Larissa Fabiana F S <ferreira-larissa@outlook.com>
2018-08-03 10:03:58 -0300

    data alterado, filtro iniciado, imagens adicionadas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
 // Add your JavaScript
-// $(document).ready(function() {
-//   $('.name-foodmap').delay('5000').fadeToggle('slow')
-// });
 $(document).ready(function() {
   $('.name-foodmap').delay('5000').fadeToggle('slow');
   $('#map').delay('5000').fadeToggle('slow');
-  getImages();
+  $('.images').delay('5000').fadeToggle('slow');
+
+  $('.search').click(function () {
+    var inputValue = $('#textFilter').val();
+    $( "img" ).each(function( ) {
+      if($(this).attr("alt") !== inputValue) {
+        $(this).fadeOut('slow');
+      }
+    });
+    }
+  )
+  // $('#textFilter').on('input', function () {
+  //   if($(this).val() === "") {
+  //     $( "img" ).attr("alt").each(function( ) {
+  //       $(this).fadeIn('slow')
+  //     });
+  //   }
+  // })
+
+
+  
 });
 
 function initMap() {
@@ -16,14 +33,35 @@ var map = new google.maps.Map(
     document.getElementById('map'), {zoom: 4, center: uluru});
 // The marker, positioned at Uluru
 var marker = new google.maps.Marker({position: uluru, map: map});
-
 }
 
-function getImages(){
-  for(var restaurante in restaurantes){
-    // console.log(res);
-    var img = restaurantes[restaurante]['image'];
-      console.log(img);
-    
-  }
-}
+
+
+
+
+// function getImages(){
+  // for(var restaurante in restaurantes){
+    // console.log(res); esse mostra cada obj restaurante
+    // var img = restaurantes[restaurante]['image'];
+      // console.log(img); esse pega cada caminho de imagem
+      // $(".images").append($('<img></img>').attr("src", img));
+  // }
+// }
+
+
+// function imagesIncrement(){
+//   for(var restaurante in restaurantes){
+//     // console.log(res); esse mostra cada obj restaurante
+//     var typeFood = restaurantes[restaurante]['type'];
+//     // console.log(typeFood);
+//     // if( ===""){
+//     //   getImages();
+//     // }
+
+//   }
+// }
+
+// function filterElements(){
+//  var text = $('#textFilter').val;
+
+// }
