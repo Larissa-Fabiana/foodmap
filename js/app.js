eadd39c38fa83f4085c237d8c69edec2a27f8667
Larissa-Fabiana <ferreira-larissa@outlook.com>
2018-08-03 19:05:06 -0300

    animação, pins e filtro de imagens

--- js/app.js
+++ js/app.js
@@ -3,65 +3,72 @@ $(document).ready(function() {
   $('.name-foodmap').delay('5000').fadeToggle('slow');
   $('#map').delay('5000').fadeToggle('slow');
   $('.images').delay('5000').fadeToggle('slow');
-
-  $('.search').click(function () {
-    var inputValue = $('#textFilter').val();
-    $( "img" ).each(function( ) {
-      if($(this).attr("alt") !== inputValue) {
-        $(this).fadeOut('slow');
-      }
-    });
-    }
-  )
-  // $('#textFilter').on('input', function () {
-  //   if($(this).val() === "") {
-  //     $( "img" ).attr("alt").each(function( ) {
-  //       $(this).fadeIn('slow')
-  //     });
-  //   }
-  // })
-
-
-  
+  getImages();
+  var init = "";
+  pinsIncrement(init);
+  $('.search').click(function(){
+    printInfo();
+  });
 });
+function printInfo() {
+  var inputValue = $('#textFilter').val();
+  $( "img" ).each(function( ) {
+    if($(this).attr("alt") !== inputValue) {
+      $(this).fadeOut('slow');
+    }
+    if(inputValue === ""){
+      $(this).fadeIn('slow');
+    }
+  });
 
-function initMap() {
-  // The location of locationMap
-var uluru = {lat: -25.344, lng: 131.036};
-// The map, centered at Uluru
-var map = new google.maps.Map(
-    document.getElementById('map'), {zoom: 4, center: uluru});
-// The marker, positioned at Uluru
-var marker = new google.maps.Marker({position: uluru, map: map});
+  pinsIncrement(inputValue);
+}
+function getImages(){
+  for(var restaurante in restaurantes){
+    // console.log(res); esse mostra cada obj restaurante
+    var img = restaurantes[restaurante]['image'];
+    var types = restaurantes[restaurante]['type'];
+      // console.log(img); esse pega cada caminho de imagem
+      var tagImg = $('<img>').attr("src", img).attr("alt", types);
+      $(".images").append(tagImg);
+
+  }
+}
+var locations = [];
+
+function pinsIncrement(inputValue){
+  for(var restaurante in restaurantes){
+    var position = [];
+    var name = restaurantes[restaurante]['name'];
+    var lat = restaurantes[restaurante]['latitude'];
+    var long = restaurantes[restaurante]['longitude'];
+    var typeFood = restaurantes[restaurante]['type'];
+    if(typeFood === inputValue || inputValue === ""){
+      position.push(name, lat, long);
+      locations.push(position);
+    }
+  }
+  initMap();
 }
 
-
-
-
-
-// function getImages(){
-  // for(var restaurante in restaurantes){
-    // console.log(res); esse mostra cada obj restaurante
-    // var img = restaurantes[restaurante]['image'];
-      // console.log(img); esse pega cada caminho de imagem
-      // $(".images").append($('<img></img>').attr("src", img));
-  // }
-// }
-
-
-// function imagesIncrement(){
-//   for(var restaurante in restaurantes){
-//     // console.log(res); esse mostra cada obj restaurante
-//     var typeFood = restaurantes[restaurante]['type'];
-//     // console.log(typeFood);
-//     // if( ===""){
-//     //   getImages();
-//     // }
-
-//   }
-// }
-
-// function filterElements(){
-//  var text = $('#textFilter').val;
-
-// }
+function initMap() {
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 15,
+  center: new google.maps.LatLng(-23.557337, -46.662460),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+for (i = 0; i < locations.length; i++) {  
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+    map: map
+  });
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
+      infowindow.setContent(locations[i][0]);
+      infowindow.open(map, marker);
+    }
+  })(marker, i));
+}
+}
